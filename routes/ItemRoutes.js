5b6f8835cbe90cb23d5eb8325b00c5a6211e2177
ying2212 <gohguanying@gmail.com>
2025-12-10 20:22:41 -0500

    add endpoint to item router

--- routes/ItemRoutes.js
+++ routes/ItemRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createItemPost, getItems} from "../handlers/ItemRoutesHandler.js";
+import { createItemPost, getItems,deletePost,updatePost} from "../handlers/ItemRoutesHandler.js";
 import { authMiddleWare } from "../middleware/auth.js";
 
 const router= express.Router();
 
 router.post("/post-lost-item", authMiddleWare,createItemPost);
 router.get("/retrieve-item-posts", getItems);
-
+router.delete("/delete-item-post/:id", deletePost);
+router.put("/update-item-post/:id", updatePost);
 export default router
